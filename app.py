25f5a87710c0abbc2cc0a031f007dfb88b996cba
arshsinghphd <arshsinghphd@gmail.com>
2020-04-05 16:59:13 -0700

    Changing bokeh method, using ColumnDataSource now.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.palettes import Spectral11
 from bokeh.embed import components 
@@ -21,22 +22,31 @@ def index():
 @app.route('/graph', methods=['POST'])
 def graph():
   if request.method == 'POST':
-##    app.vars['ticker'] = request.form['ticker']
-##    quandl.ApiConfig.api_key ="SV_rnAS4EL65RG_ZMHGh"
-##    df=quandl.get("WIKI/%s" % app.vars['ticker'])
-##    df.reset_index(inplace=True)
-##    pd.to_datetime(df['Date'],infer_datetime_format=True)
-##    p = figure(title='Stock prices for %s' % app.vars['ticker'], x_axis_label='Date', x_axis_type='datetime')
+    app.vars['ticker'] = request.form['ticker']
+    quandl.ApiConfig.api_key ="SV_rnAS4EL65RG_ZMHGh"
+    df=quandl.get("WIKI/%s" % app.vars['ticker'])
+    df.reset_index(inplace=True)
+    pd.to_datetime(df['Date'],infer_datetime_format=True)
+    source=ColumnDataSource(df)
+    p = figure(title='Stock prices for %s' % app.vars['ticker'], x_axis_label='Date', x_axis_type='datetime')
+    if request.form.get('Close'):
+        p.line(x='Date', y='Close',source=source, line_width=2, line_color="green", legend_label='Close')
+    if request.form.get('Adj. Close'):
+        p.line(x='Date', y='Adj. Close',source=source, line_width=2, line_color="black", legend_label='Adj. Close')
+    if request.form.get('Open'):
+        p.line(x='Date', y='Open',source=source, line_width=2, line_color="red", legend_label='Open')
+    if request.form.get('Adj. Open'):
+        p.line(x='Date', y='Adj. Open',source=source, line_width=2, line_color="blue", legend_label='Adj. Open') 
 ##    if request.form.get('Close'):
-##        p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, legend_label='Close')
+##        p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, line_color="green", legend_label='Close')
 ##    if request.form.get('Adj. Close'):
 ##        p.line(x=df['Date'].values, y=df['Adj. Close'].values,line_width=2, line_color="green", legend_label='Adj. Close')
 ##    if request.form.get('Open'):
 ##        p.line(x=df['Date'].values, y=df['Open'].values,line_width=2, line_color="red", legend_label='Open')
 ##    if request.form.get('Adj. Open'):
 ##        p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend_label='Adj. Open')
-    p = figure()
-    p.circle([1,2], [3,4])
+##    p = figure()
+##    p.circle([1,2], [3,4])
     script, div = components(p)
     return render_template('graph.html', script=script, div=div)
   
