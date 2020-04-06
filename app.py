f1ddc6d224672739c7652f21bbf9d82c1883fdfa
arshsinghphd <arshsinghphd@gmail.com>
2020-04-05 19:53:19 -0700

    Checking app

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import simplejson as json
 
 app = Flask(__name__)
 app.vars={}
+
 @app.route('/')
 def main():
   return redirect('/index')
@@ -28,16 +29,6 @@ def graph():
     df.reset_index(inplace=True)
     pd.to_datetime(df['Date'],infer_datetime_format=True)
 
-##    p = figure(title='Stock prices for %s' % app.vars['ticker'], x_axis_label='Date', x_axis_type='datetime')
-##    if request.form.get('Close'):
-##        p.line(df.get('Date'), df.get('Close'), line_width=2, line_color="green", legend_label='Close')
-##    if request.form.get('Adj. Close'):
-##        p.line(df.get('Date'), df.get('Adj. Close'), line_width=2, line_color="black", legend_label='Adj. Close')
-##    if request.form.get('Open'):
-##        p.line(df.get('Open'), df.get('Open'), line_width=2, line_color="red", legend_label='Open')
-##    if request.form.get('Adj. Open'):
-##        p.line(df.get('Adj. Open'), df.get('Adj. Open'), line_width=2, line_color="blue", legend_label='Adj. Open')
-
 #   Plotting using ColumnDataSource       
     source=ColumnDataSource(df)
     p = figure(title='Stock prices for %s' % app.vars['ticker'], x_axis_label='Date', x_axis_type='datetime')
@@ -49,18 +40,6 @@ def graph():
         p.line(x='Date', y='Open',source=source, line_width=2, line_color="red", legend_label='Open')
     if request.form.get('Adj. Open'):
         p.line(x='Date', y='Adj. Open',source=source, line_width=2, line_color="blue", legend_label='Adj. Open') 
-
-###  Earlier plotting
-##    source=ColumnDataSource(df)
-##    p = figure(title='Stock prices for %s' % app.vars['ticker'], x_axis_label='Date', x_axis_type='datetime')
-##    if request.form.get('Close'):
-##        p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, line_color="green", legend_label='Close')
-##    if request.form.get('Adj. Close'):
-##        p.line(x=df['Date'].values, y=df['Adj. Close'].values,line_width=2, line_color="green", legend_label='Adj. Close')
-##    if request.form.get('Open'):
-##        p.line(x=df['Date'].values, y=df['Open'].values,line_width=2, line_color="red", legend_label='Open')
-##    if request.form.get('Adj. Open'):
-##        p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend_label='Adj. Open')
 
     script, div = components(p)
     return render_template('graph.html', script=script, div=div)
